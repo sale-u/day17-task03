b4a8a75ba5925e3b44ca914191ca09b386cec933
Sasa Ucek <sale_u@hotmail.com>
2022-02-25 11:46:38 +0100

    Added User and UserType

--- UserAdmin/src/domain/User.java
+++ UserAdmin/src/domain/User.java
@@ -1,5 +1,55 @@
 package domain;
 
 public class User {
+	
+	private final String ime;
+	private final String prezime;
+	private final String username;
+	private final String password;
+	private final UserType userType;
+	
+	
+	public User(String ime, String prezime, String username, String password, UserType userType) {
+		super();
+		this.ime = ime;
+		this.prezime = prezime;
+		this.username = username;
+		this.password = password;
+		this.userType = userType;
+	}
+
+
+	public String getIme() {
+		return ime;
+	}
+
+
+	public String getPrezime() {
+		return prezime;
+	}
+
+
+	public String getUsername() {
+		return username;
+	}
+
+
+	public String getPassword() {
+		return password;
+	}
+
+
+	public UserType getUserType() {
+		return userType;
+	}
+
+
+	@Override
+	public String toString() {
+		return "User [ime=" + ime + ", prezime=" + prezime + ", username=" + username + "]";
+	}
+	
+	
+	
 
 }
