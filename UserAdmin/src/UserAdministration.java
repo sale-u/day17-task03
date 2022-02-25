85bce2ac7a1896082f2620e690c21ae78ca5bcb7
Mateja Zec <mateja.zec@eng.it>
2022-02-25 12:30:43 +0100

    addUser method added

--- UserAdmin/src/UserAdministration.java
+++ UserAdmin/src/UserAdministration.java
@@ -15,4 +15,29 @@ public class UserAdministration {
 	public List<User> getAllUsers(){
 		return this.userList;
 	}
+	
+	public boolean addUser(User user) {
+		for(User u: userList) {
+			if(u.getName().equals(user.getName())) {
+				System.out.println("User with name "+user.getName()+" already exists");
+				return false;
+			}
+		}
+		this.userList.add(user);
+		System.out.println("User "+user.getName()+" successfully added");
+		return true;
+	}
+	
+	public boolean updateUser(User user) {
+		if(containsUser(user)) {
+			//User userToChange= userList.get(userList.indexOf(user));
+		}
+		return false;
+	}
+	
+	
+	
+	private boolean containsUser(User user) {
+		return userList.contains(user);
+	}
 }
