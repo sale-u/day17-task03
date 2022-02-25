35e1968abb6402ac210c820fea48b52799edcc3e
Mateja Zec <mateja.zec@eng.it>
2022-02-25 11:50:51 +0100

    userAdmin added

--- UserAdmin/src/UserAdministration.java
+++ UserAdmin/src/UserAdministration.java
@@ -1,4 +1,13 @@
+import domain.User;
 
 public class UserAdministration {
-
+	private List<User> userList;
+	
+	public UserAdministration() {
+		super();
+	}
+	
+	public UserAdministration(List<User> userList) {
+		this.userList= userList;
+	}
 }
